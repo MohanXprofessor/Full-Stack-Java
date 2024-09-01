31f2c8b5a9ac2d58fc24fe10bf039a365b73c3ae
MohanXprofessor <yadavmohan8600@gmail.com>
2024-09-01 14:47:50 +0530

    github

---
+++ Pattern/src/com/pattern/Triangle.java
@@ -0,0 +1,21 @@
+package com.pattern;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		
+		
+		int star=5;
+		
+		for(int i=0;i<star;i++)
+		{
+			for(int j=i;j<star;j++)
+			{
+				
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+	}
+
+}
